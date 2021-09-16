2c3403e8275e8589ca0b3b7b33fa8853e6bf3b61
Parmesaaan <56564386+Parmesaaan@users.noreply.github.com>
2021-09-16 16:48:57 +0100

    Add project files.

---
+++ Project Eagles/Program.cs
@@ -0,0 +1,32 @@
+﻿using DSharpPlus;
+using System.Threading.Tasks;
+
+namespace Project_Eagles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "", // Insert token here
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged
+            });
+
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().Contains("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
